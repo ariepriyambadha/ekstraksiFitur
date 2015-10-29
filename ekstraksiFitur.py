794cb2a10b88f4bffb32e652cf74277ee7193f93
ariepriyambadha <arie.priyambadha@gmail.com>
2015-10-30 05:45:37 +0700

    commit

--- ekstraksiFitur.py
+++ ekstraksiFitur.py
@@ -1,5 +1,5 @@
+#get domain name from raw url
 def getDomainName(url):
-    # Asumsi setiap url yang ada memiliki substring http://
     indexDoubleSlash = url.find("/") + 1
     subUrl = url[indexDoubleSlash + 1:]
     if "/" in subUrl:
@@ -10,11 +10,12 @@ def getDomainName(url):
     return domainName
 
 
+#fake user agents
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1"}
 
+#get database
 with open("dataset.txt", "r") as file:
     data = file.readlines()
 
 n = 0
 while n < len(data):
-
