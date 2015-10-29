5a8509e61c2efb0b4b72cd885239d9b49268d3b1
ariepriyambadha <arie.priyambadha@gmail.com>
2015-10-30 03:53:47 +0700

    first commit bismillah

---
+++ ekstraksiFitur.py
@@ -0,0 +1,21 @@
+def getDomainName(url):
+    # Asumsi setiap url yang ada memiliki substring http://
+    indexDoubleSlash = url.find("/") + 1
+    subUrl = url[indexDoubleSlash + 1:]
+    if "/" in subUrl:
+        domainName = subUrl[:subUrl.find("/")]
+    else:
+        domainName = subUrl
+
+    return domainName
+
+
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1"}
+
+with open("dataset.txt", "r") as file:
+    data = file.readlines()
+
+n = 0
+while n < len(data):
+    print "asdf"
+    print "asdfasdfasdfasdf"
